d1cc6bb2f5fa722bf08a27c4f50286fd7450bf12
bescherkilani@gmail.com <bescherkilani@gmail.com>
2025-08-25 19:50:36 +0200

    replace useEffect with dataloader

--- src/Pages/JobPage.jsx
+++ src/Pages/JobPage.jsx
@@ -1,34 +1,19 @@
 import React from 'react';
-import {useState, useEffect} from 'react'
-import {useParams} from 'react-router-dom'
-import Spinner from "../components/Spinner.jsx";
+import {useLoaderData} from 'react-router-dom'
+
 
 const JobPage = () => {
-    const {id} = useParams();
-    const [job, setJob] = useState(null);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const fetchJob = async () => {
-            try {
-                const res = await fetch(`/api/job/${id}`);
-                const data = await res.json();
-                setJob(data);
-            } catch (error){
-                console.log('Error fetching data',error);
-            } finally {
-                setLoading(false);
-            }
-        }
-        fetchJob();
-    },[])
+    const job = useLoaderData();
 
   return (
-    loading ? <Spinner /> : (
-        <h1>JOB</h1>
-    )
+        <h1>{job.title}</h1>
   );
 };
 
+const jobLoader = async ({params}) => {
+    const res = await fetch(`/api/jobs/${params.id}`)
+    const data = await res.json();
+    return data;
+}
 
-export default JobPage;
+export {JobPage as default, jobLoader };
