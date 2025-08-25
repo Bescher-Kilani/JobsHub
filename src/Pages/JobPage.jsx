0f7e6fa6250ddf67a0716caeee546b014835bb0a
bescherkilani@gmail.com <bescherkilani@gmail.com>
2025-08-25 19:16:51 +0200

    create single job page

---
+++ src/Pages/JobPage.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {useState, useEffect} from 'react'
+
+
+const JobPage = () => {
+    const [job, setJob] = useState(null);
+
+    useEffect(() => {
+        const fetchJob = async () => {
+            try {
+                const res = await fetch(apiURL);
+                const data = await res.json();
+                setJobs(data);
+            } catch (error){
+                console.log('Error fetching data',error);
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchJob();
+    },[])
+  return (
+    <div>JobPage</div>
+  );
+};
+
+
+export default JobPage;
