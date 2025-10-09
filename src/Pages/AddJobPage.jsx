eafe0152155c93c0370512476f74727bd26212b4
bescherkilani@gmail.com <bescherkilani@gmail.com>
2025-10-09 15:04:35 +0200

    AddJobPage change to german

--- src/Pages/AddJobPage.jsx
+++ src/Pages/AddJobPage.jsx
@@ -6,10 +6,10 @@ import{toast} from "react-toastify";
 
 const AddJobPage = ({addJobSubmit}) => {
     const [title, setTitle] = useState('');
-    const [type, setType] = useState('Full-Time');
+    const [type, setType] = useState('Vollzeit');
     const [location, setLocation] = useState('');
     const [description, setDescription] = useState('');
-    const [salary, setSalary] = useState('Under $50K');
+    const [salary, setSalary] = useState('Unter 50K€');
     const [companyName, setCompanyName] = useState('');
     const [companyDescription, setCompanyDescription] = useState('');
     const [contactEmail, setContactEmail] = useState('');
@@ -45,11 +45,11 @@ const AddJobPage = ({addJobSubmit}) => {
                   className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
               >
                   <form onSubmit={submitForm}>
-                      <h2 className="text-3xl text-center font-semibold mb-6">Add Job</h2>
+                      <h2 className="text-3xl text-center font-semibold mb-6">Job Hinzufügen</h2>
 
                       <div className="mb-4">
                           <label htmlFor="type" className="block text-gray-700 font-bold mb-2"
-                          >Job Type</label
+                          >Job Typ</label
                           >
                           <select
                               id="type"
@@ -59,23 +59,23 @@ const AddJobPage = ({addJobSubmit}) => {
                               value={type}
                               onChange={(e) => setType(e.target.value)}
                           >
-                              <option value="Full-Time">Full-Time</option>
-                              <option value="Part-Time">Part-Time</option>
+                              <option value="Full-Time">Vollzeit</option>
+                              <option value="Part-Time">Teilzeit</option>
                               <option value="Remote">Remote</option>
-                              <option value="Internship">Internship</option>
+                              <option value="Internship">Praktikum</option>
                           </select>
                       </div>
 
                       <div className="mb-4">
                           <label className="block text-gray-700 font-bold mb-2"
-                          >Job Listing Name</label
+                          >Job Name</label
                           >
                           <input
                               type="text"
                               id="title"
                               name="title"
                               className="border rounded w-full py-2 px-3 mb-2"
-                              placeholder="eg. Beautiful Apartment In Miami"
+                              placeholder="z.B Frontend Developer"
                               required
                               value={title}
                               onChange={(e) => setTitle(e.target.value)}
@@ -85,14 +85,14 @@ const AddJobPage = ({addJobSubmit}) => {
                           <label
                               htmlFor="description"
                               className="block text-gray-700 font-bold mb-2"
-                          >Description</label
+                          >Beschreibung</label
                           >
                           <textarea
                               id="description"
                               name="description"
                               className="border rounded w-full py-2 px-3"
                               rows="4"
-                              placeholder="Add any job duties, expectations, requirements, etc"
+                              placeholder="Fügen Sie alle Arbeitsaufgaben, Erwartungen, Anforderungen usw. hinzu"
                               value={description}
                               onChange={(e) => setDescription(e.target.value)}
                           ></textarea>
@@ -100,7 +100,7 @@ const AddJobPage = ({addJobSubmit}) => {
 
                       <div className="mb-4">
                           <label htmlFor="type" className="block text-gray-700 font-bold mb-2"
-                          >Salary</label
+                          >Gehalt</label
                           >
                           <select
                               id="salary"
@@ -110,48 +110,49 @@ const AddJobPage = ({addJobSubmit}) => {
                               value={salary}
                               onChange={(e) => setSalary(e.target.value)}
                           >
-                              <option value="Under $50K">Under $50K</option>
-                              <option value="$50K - 60K">$50K - $60K</option>
-                              <option value="$60K - 70K">$60K - $70K</option>
-                              <option value="$70K - 80K">$70K - $80K</option>
-                              <option value="$80K - 90K">$80K - $90K</option>
-                              <option value="$90K - 100K">$90K - $100K</option>
-                              <option value="$100K - 125K">$100K - $125K</option>
-                              <option value="$125K - 150K">$125K - $150K</option>
-                              <option value="$150K - 175K">$150K - $175K</option>
-                              <option value="$175K - 200K">$175K - $200K</option>
-                              <option value="Over $200K">Over $200K</option>
+                              <option value="Unter 50.000 €">Unter 50.000 €</option>
+                              <option value="50.000–60.000 €">50.000–60.000 €</option>
+                              <option value="60.000–70.000 €">60.000–70.000 €</option>
+                              <option value="70.000–80.000 €">70.000–80.000 €</option>
+                              <option value="80.000–90.000 €">80.000–90.000 €</option>
+                              <option value="90.000–100.000 €">90.000–100.000 €</option>
+                              <option value="100.000–125.000 €">100.000–125.000 €</option>
+                              <option value="125.000–150.000 €">125.000–150.000 €</option>
+                              <option value="150.000–175.000 €">150.000–175.000 €</option>
+                              <option value="175.000–200.000 €">175.000–200.000 €</option>
+                              <option value="Über 200.000 €">Über 200.000 €</option>
+
                           </select>
                       </div>
 
                       <div className='mb-4'>
                           <label className='block text-gray-700 font-bold mb-2'>
-                              Location
+                              Ort
                           </label>
                           <input
                               type='text'
                               id='location'
                               name='location'
                               className='border rounded w-full py-2 px-3 mb-2'
-                              placeholder='Company Location'
+                              placeholder='Firmenstandort'
                               required
                               value={location}
                               onChange={(e) => setLocation(e.target.value)}
                           />
                       </div>
 
-                      <h3 className="text-2xl mb-5">Company Info</h3>
+                      <h3 className="text-2xl mb-5">Firma Info</h3>
 
                       <div className="mb-4">
                           <label htmlFor="company" className="block text-gray-700 font-bold mb-2"
-                          >Company Name</label
+                          >Firmenname</label
                           >
                           <input
                               type="text"
                               id="company"
                               name="company"
                               className="border rounded w-full py-2 px-3"
-                              placeholder="Company Name"
+                              placeholder="Firmenname"
                               value={companyName}
                               onChange={(e) => setCompanyName(e.target.value)}
                           />
@@ -161,14 +162,14 @@ const AddJobPage = ({addJobSubmit}) => {
                           <label
                               htmlFor="company_description"
                               className="block text-gray-700 font-bold mb-2"
-                          >Company Description</label
+                          >Firmenbeschreibung</label
                           >
                           <textarea
                               id="company_description"
                               name="company_description"
                               className="border rounded w-full py-2 px-3"
                               rows="4"
-                              placeholder="What does your company do?"
+                              placeholder="Was macht Ihre Firma?"
                               value={companyDescription}
                               onChange={(e) => setCompanyDescription(e.target.value)}
                           ></textarea>
@@ -178,14 +179,14 @@ const AddJobPage = ({addJobSubmit}) => {
                           <label
                               htmlFor="contact_email"
                               className="block text-gray-700 font-bold mb-2"
-                          >Contact Email</label
+                          >Email</label
                           >
                           <input
                               type="email"
                               id="contact_email"
                               name="contact_email"
                               className="border rounded w-full py-2 px-3"
-                              placeholder="Email address for applicants"
+                              placeholder="Email für Bewerber"
                               required
                               value={contactEmail}
                               onChange={(e) => setContactEmail(e.target.value)}
@@ -195,14 +196,14 @@ const AddJobPage = ({addJobSubmit}) => {
                           <label
                               htmlFor="contact_phone"
                               className="block text-gray-700 font-bold mb-2"
-                          >Contact Phone</label
+                          >Mobilnummer</label
                           >
                           <input
                               type="tel"
                               id="contact_phone"
                               name="contact_phone"
                               className="border rounded w-full py-2 px-3"
-                              placeholder="Optional phone for applicants"
+                              placeholder="Optional Mobilnummer"
                               value={contactPhone}
                               onChange={(e) => setContactPhone(e.target.value)}
                           />
@@ -213,7 +214,7 @@ const AddJobPage = ({addJobSubmit}) => {
                               className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
                               type="submit"
                           >
-                              Add Job
+                             Job Hinzufügen
                           </button>
                       </div>
                   </form>
