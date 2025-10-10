9acb7902a6370f0468b9f2320cb1f8ade62e0a67
bescherkilani@gmail.com <bescherkilani@gmail.com>
2025-10-10 18:17:40 +0200

    HomeCard and JobListings change to german

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -8,27 +8,28 @@ const HomeCards = () => {
           <div className="container-xl lg:container m-auto">
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
                   <Card>
-                      <h2 className="text-2xl font-bold">For Developers</h2>
+                      <h2 className="text-2xl font-bold">Für Entwickler</h2>
                       <p className="mt-2 mb-4">
-                          Browse our React jobs and start your career today
+                          Durchsuche unsere React-Jobs und starte noch heute deine Karriere
+
                       </p>
                       <Link
                           to="/jobs"
                           className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
                       >
-                          Browse Jobs
+                          Jobs Durchsuchen
                       </Link>
                   </Card>
                   <Card bg="bg-indigo-100">
-                      <h2 className="text-2xl font-bold">For Employers</h2>
+                      <h2 className="text-2xl font-bold">Für Arbeitgeber</h2>
                       <p className="mt-2 mb-4">
-                          List your job to find the perfect developer for the role
+                          Veröffentliche deinen Job und finde den idealen React-Entwickler für dein Team
                       </p>
                       <Link
                           to="/add-job"
                           className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
                       >
-                          Add Job
+                          Job Hinzufügen
                       </Link>
                   </Card>
               </div>
